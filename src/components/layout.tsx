97e01cc862fbc266b450d75b812dd3a03dce0336
slawiko <shchaurouski.slava@gmail.com>
2022-07-14 18:46:00 +0200

    Basic email subscription

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,3 +1,4 @@
+import { API } from "aws-amplify";
 import * as React from "react";
 import { Trans } from "react-i18next";
 
@@ -6,14 +7,47 @@ import { Footer } from "./footer";
 import { section } from "./layout.css";
 
 export function Layout(): JSX.Element {
+  const emailInputId = "email-input";
+  const submit = (event: React.MouseEvent) => {
+    event.preventDefault();
+
+    const emailInput = document.getElementById(emailInputId) as HTMLInputElement;
+    if (!isValidInput(emailInput)) {
+      return;
+    }
+
+    API.post("subscribe", "/subscribe", {
+      body: { email: emailInput.value },
+    })
+      .then((resp) => {
+        console.log("subscribed", resp);
+      })
+      .catch((e) => {
+        console.error(e.toJSON());
+      });
+  };
+
   return (
     <>
       <Header className={section} />
       <section className={section}>
         <Trans>subscribe</Trans>
-        <input type="text" />
+        <input id={emailInputId} name="email" type="text" />
+        <button onClick={submit}>
+          <Trans>subscribe-button</Trans>
+        </button>
       </section>
       <Footer className={section} />
     </>
   );
+}
+
+function isValidInput(input: HTMLInputElement): boolean {
+  if (!input || !input.value) {
+    return false;
+  }
+
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(input.value).toLowerCase());
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { Helmet } from "react-helmet";
 import { graphql } from "gatsby";
+import { Amplify } from "aws-amplify";
 
 import { Layout } from "../components/layout";
 
@@ -17,6 +18,17 @@ const IndexPage = () => {
 };
 
 export default IndexPage;
+
+Amplify.configure({
+  API: {
+    endpoints: [
+      {
+        name: "subscribe",
+        endpoint: "https://x2z9ll0k6i.execute-api.eu-central-1.amazonaws.com/staging",
+      },
+    ],
+  },
+});
 
 export const query = graphql`
   query ($language: String!) {
