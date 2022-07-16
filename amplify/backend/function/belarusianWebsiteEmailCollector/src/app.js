469eaf4325854c300cd879d47ea04ac5fa8cd2bf
slawiko <shchaurouski.slava@gmail.com>
2022-07-16 13:26:36 +0200

    Enables CORS for lambda

--- amplify/backend/function/belarusianWebsiteEmailCollector/src/app.js
+++ amplify/backend/function/belarusianWebsiteEmailCollector/src/app.js
@@ -21,10 +21,10 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use(function (req, res, next) {
-  // res.header("Access-Control-Allow-Origin", "belarusians.nl");
-  // res.header("Access-Control-Allow-Headers", "belarusians.nl");
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Origin", "belarusians.nl");
+  res.header("Access-Control-Allow-Headers", "belarusians.nl");
+  // res.header("Access-Control-Allow-Origin", "*");
+  // res.header("Access-Control-Allow-Headers", "*");
   next();
 });
 
