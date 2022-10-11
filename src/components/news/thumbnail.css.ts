eba081be83668eb17de6ff413db7aa3eca8217d4
slawiko <shchaurouski.slava@gmail.com>
2022-10-11 17:13:58 +0200

    Makes thumbnail text smaller on small devices

--- src/components/news/thumbnail.css.ts
+++ src/components/news/thumbnail.css.ts
@@ -2,15 +2,23 @@ import { style } from "@vanilla-extract/css";
 import { sprinkles } from "../sprinkles.css";
 import { vars } from "../styles.css";
 
-export const title = style([{
-  bottom: "0.5rem",
-  left: "1rem",
-  fontSize: "2.5rem",
-  position: "absolute",
-  textTransform: "uppercase",
-  textShadow: "-1px 0px red, 1px 0px red, 0px 1px red, 0px -1px red",
-  color: vars.color.white,
-}]);
+export const title = style([
+  sprinkles({
+    fontSize: {
+      sm: "1.5rem",
+      md: "2rem",
+      lg: "2.5rem",
+    }
+  }),
+  {
+    bottom: "0.5rem",
+    left: "1rem",
+    position: "absolute",
+    textTransform: "uppercase",
+    textShadow: "-1px 0px red, 1px 0px red, 0px 1px red, 0px -1px red",
+    color: vars.color.white,
+  },
+]);
 
 export const image = style([{
   borderRadius: "5px",
