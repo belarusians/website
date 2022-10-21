a647cf2d2f212036d0bb97bd297d5b472977c13c
slawiko <shchaurouski.slava@gmail.com>
2022-10-21 11:16:19 +0200

    Fixes jumping FontAwesome

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,14 @@ import { AppProps } from "next/app";
 import { appWithTranslation } from "next-i18next";
 import { themeClass } from "../components/styles.css";
 
+/**
+ * workaround for flickering FA icon
+ * see https://github.com/FortAwesome/react-fontawesome/issues/234
+ */
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '../../node_modules/@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className={themeClass}>
