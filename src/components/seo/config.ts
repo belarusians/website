9dc0533c6a57e22ce1ce2dd762fa39bc1a88e094
slawiko <shchaurouski.slava@gmail.com>
2022-10-27 16:48:54 +0200

    Adds different configuration for different environments

--- src/components/seo/config.ts
+++ src/components/seo/config.ts
@@ -1,6 +1,17 @@
-export default {
-  title: "Mara - Belarusians NL",
+const next = {
+  title: "Mara NEXT",
+  description: "Official website of Belarusian diaspora in the Netherlands - MARA",
+  siteUrl: "https://next.belarusians.nl",
+  imagePath: "/logo/og-image.jpg",
+};
+
+const prod = {
+  title: "Mara",
   description: "Official website of Belarusian diaspora in the Netherlands - MARA",
   siteUrl: "https://belarusians.nl",
   imagePath: "/logo/og-image.jpg",
 };
+
+const configuration = process.env.APP_ENV === 'prod' ? prod : next;
+
+export default configuration;
