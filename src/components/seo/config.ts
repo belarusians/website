e9f864dbd23651dfec3ffa92275cd6f15019be50
slawiko <shchaurouski.slava@gmail.com>
2022-11-07 13:51:33 +0100

    Moves Favicon-related stuff in separate component

--- src/components/seo/config.ts
+++ src/components/seo/config.ts
@@ -1,17 +1,27 @@
-const next = {
+interface SEOConfiguration {
+  title: string;
+  description: string;
+  siteUrl: string;
+  domain: string;
+  imagePath: string;
+}
+
+const next: SEOConfiguration = {
   title: "Mara NEXT",
   description: "Official website of Belarusian diaspora in the Netherlands - MARA",
   siteUrl: "https://next.belarusians.nl",
+  domain: "next.belarusians.nl",
   imagePath: "/logo/og-image.jpg",
 };
 
-const prod = {
+const prod: SEOConfiguration = {
   title: "Mara",
   description: "Official website of Belarusian diaspora in the Netherlands - MARA",
   siteUrl: "https://belarusians.nl",
+  domain: "belarusians.nl",
   imagePath: "/logo/og-image.jpg",
 };
 
-const configuration = process.env.APP_ENV === 'prod' ? prod : next;
+const configuration: SEOConfiguration = process.env.APP_ENV === 'prod' ? prod : next;
 
 export default configuration;
