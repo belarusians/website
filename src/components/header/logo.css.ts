9488a5ae8e835bc5c043da5305a1b34e98a87cb6
slawiko <shchaurouski.slava@gmail.com>
2023-01-13 18:58:52 +0100

    Adds possibility to make Logo white

--- src/components/header/logo.css.ts
+++ src/components/header/logo.css.ts
@@ -2,20 +2,21 @@ import { style } from "@vanilla-extract/css";
 import { vars } from "../styles.css";
 import { sprinkles } from "../sprinkles.css";
 
-export const logo = style({
+export const redColor = style({
   fill: vars.color.red,
+});
+
+export const whiteColor = style({
+  fill: vars.color.white,
 });
 
 export const bottomText = style([
   sprinkles({
     display: {
       sm: "none",
-    }
+    },
   }),
   {
-  fill: vars.color.black,
-}]);
-
-export const mainText = style([
-  logo
+    fill: vars.color.black,
+  },
 ]);
