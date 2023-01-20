78de2b6efcde2db545aa709b0d6bb80bdfb3a5b9
slawiko <shchaurouski.slava@gmail.com>
2023-01-20 20:08:42 +0100

    Removes NEXT configuration

--- src/components/seo/config.ts
+++ src/components/seo/config.ts
@@ -17,19 +17,15 @@ const prod: SEOConfiguration = {
     nl: "Nederlandse non-profit organisatie van Belarussen voor Belarussen - MARA",
     ru: "Нидерланская некоммерческая организация беларусов для беларусов - MARA",
   },
-  siteUrl: "https://belarusians.nl",
+  siteUrl: "https://www.belarusians.nl",
   domain: "belarusians.nl",
   imagePath: "/logo/og-image.jpg",
 };
 
-const next: SEOConfiguration = {
+const preview: SEOConfiguration = {
   ...prod,
-  title: "MARA NEXT",
-  siteUrl: "https://next.belarusians.nl",
-  domain: "next.belarusians.nl",
 };
 
-
-const configuration: SEOConfiguration = process.env.APP_ENV === "prod" ? prod : next;
+const configuration: SEOConfiguration = process.env.APP_ENV === "prod" ? prod : preview;
 
 export default configuration;
