70e1d62e28e23965ad4f576d5d97443ee2adf438
slawiko <shchaurouski.slava@gmail.com>
2023-01-22 23:23:30 +0100

    Improves Layout: now it will not rerender common stuff

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,14 +3,15 @@ import { AppProps } from "next/app";
 import { appWithTranslation } from "next-i18next";
 import { Analytics } from "@vercel/analytics/react";
 import { themeClass } from "../components/styles.css";
+import { Layout } from "../components/layout";
 
 /**
  * workaround for flickering FA icon
  * see https://github.com/FortAwesome/react-fontawesome/issues/234
  */
-import { config } from "@fortawesome/fontawesome-svg-core"
-import "../../node_modules/@fortawesome/fontawesome-svg-core/styles.css"
-config.autoAddCss = false
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "../../node_modules/@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
 
 const roboto = Roboto({
   weight: ["300", "400", "500", "900"],
@@ -21,7 +22,9 @@ function MyApp({ Component, pageProps }: AppProps) {
   const className = themeClass + " " +  roboto.className;
   return (
     <div className={className}>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
       <Analytics />
     </div>
   );
