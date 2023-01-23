38413a2da173c3f9531b3320e5252723859a9e47
slawiko <shchaurouski.slava@gmail.com>
2023-01-23 15:58:32 +0100

    Adds custom meta tags for join us page

--- src/components/head/head.tsx
+++ src/components/head/head.tsx
@@ -4,34 +4,41 @@ import NextHead from "next/head";
 import seo, { SEOConfiguration } from "./config";
 import { Lang } from "../types";
 
-export type HeadProps = Pick<SEOConfiguration, "title" | "imageUrl"> & {
+export type HeadProps = Pick<SEOConfiguration, "title"> & {
   description?: string;
   siteUrl?: string;
   lang?: Lang;
+  imagePath?: string;
 };
 
 export const Head = (props: Partial<HeadProps>): JSX.Element => {
+  const lang = props.lang ?? Lang.be;
+  let imageUrl = seo.imageUrl;
+  if (props.imagePath) {
+    imageUrl = `${seo.siteUrl[lang]}${props.imagePath}`;
+  }
+
   return (
     <NextHead>
       <title>{props.title ?? seo.title}</title>
 
-      <meta name="description" content={props.description ?? seo.description[props.lang ?? Lang.be]} />
-      <meta name="image" content={props.imageUrl ?? seo.imageUrl} />
+      <meta name="description" content={props.description ?? seo.description[lang]} />
+      <meta name="image" content={imageUrl} />
 
-      <meta property="og:url" content={props.siteUrl ?? seo.siteUrl[props.lang ?? Lang.be]} />
+      <meta property="og:url" content={props.siteUrl ?? seo.siteUrl[lang]} />
       <meta property="og:title" content={props.title ?? seo.title} />
       <meta property="og:type" content="website" />
-      <meta property="og:description" content={props.description ?? seo.description[props.lang ?? Lang.be]} />
-      <meta property="og:image" content={props.imageUrl ?? seo.imageUrl} />
+      <meta property="og:description" content={props.description ?? seo.description[lang]} />
+      <meta property="og:image" content={imageUrl} />
       <meta property="og:image:height" content="301" />
       <meta property="og:image:width" content="574" />
 
       <meta name="twitter:card" content="summary_large_image" />
       <meta property="twitter:domain" content={seo.domain} />
-      <meta property="twitter:url" content={props.siteUrl ?? seo.siteUrl[props.lang ?? Lang.be]} />
+      <meta property="twitter:url" content={props.siteUrl ?? seo.siteUrl[lang]} />
       <meta name="twitter:title" content={props.title ?? seo.title} />
-      <meta name="twitter:description" content={props.description ?? seo.description[props.lang ?? Lang.be]} />
-      <meta name="twitter:image" content={props.imageUrl ?? seo.imageUrl} />
+      <meta name="twitter:description" content={props.description ?? seo.description[lang]} />
+      <meta name="twitter:image" content={imageUrl} />
     </NextHead>
   );
 };
