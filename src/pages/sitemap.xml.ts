27a3806efd0e91f182d96f62b3e7a584178c3850
slawiko <shchaurouski.slava@gmail.com>
2023-01-24 13:43:23 +0100

    Adds translations in sitemap.xml

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -2,32 +2,32 @@ import { getNewsSlugs } from "../lib/news";
 import { GetServerSidePropsContext } from "next";
 import { Lang } from "../components/types";
 
+function generateTranslatedUrls(path: string): string {
+  const relativePath = path.startsWith("/") ? path : `/${path}`;
+  return `<url>
+            <loc>https://www.belarusians.nl${relativePath}</loc>
+          </url>
+          <url>
+             <loc>https://www.belarusians.nl/be${relativePath}</loc>
+          </url>
+          <url>
+             <loc>https://www.belarusians.nl/ru${relativePath}</loc>
+          </url>
+          <url>
+             <loc>https://www.belarusians.nl/nl${relativePath}</loc>
+          </url>`;
+}
+
 function generateSiteMap(newsSlugs: string[]) {
-  const newsUrls = newsSlugs
-    .map(
-      (slug) => `
-       <url>
-           <loc>https://www.belarusians.nl/news/${slug}</loc>
-       </url>`,
-    )
-    .join("");
+  const newsUrls = newsSlugs.map((slug) => generateTranslatedUrls(`/news/${slug}`)).join("");
   return `<?xml version="1.0" encoding="UTF-8"?>
-   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-     <url>
-       <loc>https://www.belarusians.nl</loc>
-     </url>
-     <url>
-       <loc>https://www.belarusians.nl/join-us</loc>
-     </url>
-     <url>
-       <loc>https://www.belarusians.nl/about-us</loc>
-     </url>
-     <url>
-       <loc>https://www.belarusians.nl/targets</loc>
-     </url>
-     ${newsUrls}
-   </urlset>
- `;
+            <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+              ${generateTranslatedUrls("")}
+              ${generateTranslatedUrls("/join-us")}
+              ${generateTranslatedUrls("/about-us")}
+              ${generateTranslatedUrls("/targets")}
+              ${newsUrls}
+            </urlset>`;
 }
 
 export default function SiteMap() {
