7bb0b3e52c7706fb83d2c408f65495251be55e7d
slawiko <shchaurouski.slava@gmail.com>
2023-01-24 20:19:13 +0100

    Adds canonicals and alternate links

--- src/components/head/head.tsx
+++ src/components/head/head.tsx
@@ -3,6 +3,7 @@ import NextHead from "next/head";
 
 import seo, { SEOConfiguration } from "./config";
 import { Lang } from "../types";
+import { useRouter } from "next/router";
 
 export type HeadProps = Pick<SEOConfiguration, "title"> & {
   description?: string;
@@ -17,16 +18,26 @@ const langToLocale = {
   ru: "ru_BY",
 };
 
+function renderCanonicalLink(path: string) {
+  const relativePath = path === "" || path.startsWith("/") ? path : `/${path}`;
+
+  return <link rel="canonical" href={`https://www.belarusians.nl${relativePath}`} />;
+}
+
 export const Head = (props: Partial<HeadProps>): JSX.Element => {
-  const lang = props.lang ?? Lang.be;
+  const currentLanguage = props.lang ?? Lang.be;
   let imageUrl = seo.imageUrl;
   if (props.imagePath) {
-    imageUrl = `${seo.siteUrl[lang]}${props.imagePath}`;
+    imageUrl = `${seo.siteUrl[currentLanguage]}${props.imagePath}`;
   }
+
+  const { asPath } = useRouter();
 
   return (
     <NextHead>
       <title>{props.title ?? seo.title}</title>
+
+      {currentLanguage === Lang.be ? renderCanonicalLink(asPath) : null}
 
       <meta
         name="keywords"
@@ -34,27 +45,31 @@ export const Head = (props: Partial<HeadProps>): JSX.Element => {
       />
       <meta name="robots" content="index, follow" />
 
-      <meta name="description" content={props.description ?? seo.description[lang]} key="meta-description" />
+      <meta name="description" content={props.description ?? seo.description[currentLanguage]} key="meta-description" />
       <meta name="image" content={imageUrl} key="meta-image" />
 
-      <meta property="og:url" content={props.siteUrl ?? seo.siteUrl[lang]} key="meta-og:url" />
+      <meta property="og:url" content={props.siteUrl ?? seo.siteUrl[currentLanguage]} key="meta-og:url" />
       <meta property="og:title" key="meta-og:title" content={props.title ?? seo.title} />
       <meta property="og:type" content="website" />
-      <meta property="og:description" key="meta-og:description" content={props.description ?? seo.description[lang]} />
+      <meta
+        property="og:description"
+        key="meta-og:description"
+        content={props.description ?? seo.description[currentLanguage]}
+      />
       <meta property="og:image" key="meta-og:image" content={imageUrl} />
       <meta property="og:site_name" key="meta-og:site_name" content={seo.siteName} />
       <meta property="og:image:height" content="301" />
       <meta property="og:image:width" content="574" />
-      <meta property="og:locale" content={langToLocale[lang]} />
+      <meta property="og:locale" content={langToLocale[currentLanguage]} />
 
       <meta name="twitter:card" content="summary_large_image" />
       <meta property="twitter:domain" key="meta-twitter:domain" content={seo.domain} />
-      <meta property="twitter:url" key="meta-twitter:url" content={props.siteUrl ?? seo.siteUrl[lang]} />
+      <meta property="twitter:url" key="meta-twitter:url" content={props.siteUrl ?? seo.siteUrl[currentLanguage]} />
       <meta name="twitter:title" key="meta-twitter:title" content={props.title ?? seo.title} />
       <meta
         name="twitter:description"
         key="meta-twitter:description"
-        content={props.description ?? seo.description[lang]}
+        content={props.description ?? seo.description[currentLanguage]}
       />
       <meta name="twitter:image" key="meta-twitter:image" content={imageUrl} />
     </NextHead>
--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -3,25 +3,31 @@ import { GetServerSidePropsContext } from "next";
 import { Lang } from "../components/types";
 
 function generateTranslatedUrls(path: string): string {
-  const relativePath = path.startsWith("/") ? path : `/${path}`;
+  const relativePath = path.startsWith("/") ? path.slice(1) : path;
   return `<url>
-            <loc>https://www.belarusians.nl${relativePath}</loc>
+            <loc>https://www.belarusians.nl/${relativePath}</loc>
+            <xhtml:link rel="alternate" hreflang="be" href="https://www.belarusians.nl/${relativePath}"/>
+            <xhtml:link rel="alternate" hreflang="nl" href="https://www.belarusians.nl/nl/${relativePath}"/>
+            <xhtml:link rel="alternate" hreflang="ru" href="https://www.belarusians.nl/ru/${relativePath}"/>
           </url>
           <url>
-             <loc>https://www.belarusians.nl/be${relativePath}</loc>
+            <loc>https://www.belarusians.nl/nl/${relativePath}</loc>
+            <xhtml:link rel="alternate" hreflang="be" href="https://www.belarusians.nl/${relativePath}"/>
+            <xhtml:link rel="alternate" hreflang="nl" href="https://www.belarusians.nl/nl/${relativePath}"/>
+            <xhtml:link rel="alternate" hreflang="ru" href="https://www.belarusians.nl/ru/${relativePath}"/>
           </url>
           <url>
-             <loc>https://www.belarusians.nl/ru${relativePath}</loc>
-          </url>
-          <url>
-             <loc>https://www.belarusians.nl/nl${relativePath}</loc>
+            <loc>https://www.belarusians.nl/ru/${relativePath}</loc>
+            <xhtml:link rel="alternate" hreflang="be" href="https://www.belarusians.nl/${relativePath}"/>
+            <xhtml:link rel="alternate" hreflang="nl" href="https://www.belarusians.nl/nl/${relativePath}"/>
+            <xhtml:link rel="alternate" hreflang="ru" href="https://www.belarusians.nl/ru/${relativePath}"/>
           </url>`;
 }
 
 function generateSiteMap(newsSlugs: string[]) {
   const newsUrls = newsSlugs.map((slug) => generateTranslatedUrls(`/news/${slug}`)).join("");
   return `<?xml version="1.0" encoding="UTF-8"?>
-            <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+            <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
               ${generateTranslatedUrls("")}
               ${generateTranslatedUrls("/join-us")}
               ${generateTranslatedUrls("/about-us")}
@@ -30,9 +36,7 @@ function generateSiteMap(newsSlugs: string[]) {
             </urlset>`;
 }
 
-export default function SiteMap() {
-  // getServerSideProps will do the heavy lifting
-}
+export default function SiteMap() {}
 
 export async function getServerSideProps({ res, locale }: GetServerSidePropsContext) {
   const news = getNewsSlugs(locale as Lang);
