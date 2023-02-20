7d04849570e67cce56087722aa55377acd6ef64c
slawiko <shchaurouski.slava@gmail.com>
2023-02-20 20:25:50 +0100

    Separate view for events

---
+++ src/components/articles/event-article.css.ts
@@ -0,0 +1,73 @@
+import { keyframes, style } from "@vanilla-extract/css";
+import { sprinkles } from "../sprinkles.css";
+import { vars } from "../styles.css";
+
+export const eventContainer = style([
+  sprinkles({
+    flexDirection: {
+      sm: "column",
+      md: "row",
+      lg: "row",
+    },
+    paddingX: {
+      sm: "large",
+      md: "large",
+      lg: "extraLarge",
+    },
+    paddingBottom: {
+      sm: "large",
+      md: "large",
+      lg: "extraLarge",
+    },
+  }),
+  {
+    display: "flex",
+    gap: "1rem",
+    lineHeight: "1.5rem",
+  },
+]);
+
+export const eventContent = style([
+  sprinkles({
+    flex: {
+      sm: "100%",
+      md: "70%",
+      lg: "80%",
+    },
+  }),
+]);
+
+export const eventButtons = style([
+  sprinkles({
+    flex: {
+      sm: "100%",
+      md: "30%",
+      lg: "20%",
+    },
+  }),
+]);
+
+const backgroundRotation = keyframes({
+  "0%": {
+    backgroundPosition: "0 0",
+  },
+  "100%": {
+    backgroundPosition: "100% 0",
+  },
+});
+
+const color = {
+  first: "#ff1111",
+  second: "#b40000",
+  third: "#ff0000",
+  fourth: "#af0000",
+};
+
+export const ticketButton = style({
+  width: "100%",
+  marginTop: "1rem",
+  color: vars.color.white,
+  backgroundImage: `linear-gradient(60deg, ${color.first}, ${color.second}, ${color.third}, ${color.fourth}, ${color.first}, ${color.second}, ${color.third})`,
+  animation: `${backgroundRotation} 4s infinite linear`,
+  backgroundSize: "350% 100%",
+});
