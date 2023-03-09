effeb294b0f3ce3869f6ced60140d3f2a6c14abd
slawiko <shchaurouski.slava@gmail.com>
2023-03-09 19:08:41 +0100

    Belarussen -> Belarusen

--- src/components/head/config.ts
+++ src/components/head/config.ts
@@ -18,13 +18,13 @@ export interface SEOConfiguration {
 const prod: SEOConfiguration = {
   title: {
     be: "MARA: Аб'яднанне беларусаў у Нідэрландах",
-    nl: "MARA: Vereniging Belarussen in Nederland",
+    nl: "MARA: Vereniging Belarusen in Nederland",
     ru: "MARA: Объединение беларусов в Нидерландах",
   },
   siteName: "MÁRA",
   description: {
     be: "MARA - гэта некамерцыйная арганізацыя неабыякавых беларусаў Нідэрландаў, якія мараць аб цудоўнай будучыні для сваёй роднай краіны.",
-    nl: "MARA is een non-profit organisatie van zorgzame Belarussen in Nederland die dromen van een mooie toekomst voor hun vaderland.",
+    nl: "MARA is een non-profit organisatie van zorgzame Belarusen in Nederland die dromen van een mooie toekomst voor hun vaderland.",
     ru: "MARA - это некоммерческая организация небезразличных беларусов Нидерландов, которые мечтают о прекрасном будущем для своей родной страны.",
   },
   siteUrl: {
