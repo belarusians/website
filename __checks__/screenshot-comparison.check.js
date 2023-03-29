816069ffbb1e5615f98c395c2dadae3ca34f7261
slawiko <shchaurouski.slava@gmail.com>
2023-03-29 11:21:48 +0200

    Adds screenshot comparison check

---
+++ __checks__/screenshot-comparison.check.js
@@ -0,0 +1,28 @@
+const { chromium } = require("playwright");
+const { expect } = require("@playwright/test");
+
+async function run() {
+  const browser = await chromium.launch();
+  const curPage = await browser.newPage();
+  const newPage = await browser.newPage();
+
+  const curUrl = "https://www.belarusians.nl";
+  const newUrl = process.env.ENVIRONMENT_URL;
+
+  const newResp = await newPage.goto(newUrl);
+
+  if (newResp.status() > 399) {
+    throw new Error(`New URL open failed with response code ${newResp.status()}`);
+  }
+
+  await curPage.goto(curUrl);
+  await curPage.screenshot({ path: "current.jpg" });
+  await expect(newPage).toHaveScreenshot("current.jpg");
+
+  await curPage.close();
+  await newPage.close();
+
+  await browser.close();
+}
+
+run();
