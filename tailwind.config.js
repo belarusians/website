6ab48d3a30dc25cc7c9da8d12c72bd2f85c0867c
slawiko <shchaurouski.slava@gmail.com>
2023-05-25 14:32:44 +0200

    TW config

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/pages/**/*.tsx"],
+  theme: {
+    colors: {
+      red: "#ed1c24",
+    },
+    fontFamily: {},
+    extend: {},
+  },
+  plugins: [],
+};
