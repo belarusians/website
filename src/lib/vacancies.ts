2821782840d9adf53f54806c205f4ad0cdabbcb1
slawiko <shchaurouski.slava@gmail.com>
2023-06-11 17:54:30 +0200

    Adds vacancies page

---
+++ src/lib/vacancies.ts
@@ -0,0 +1,27 @@
+import { Lang } from "../components/types";
+import vacancies from "../../data/vacancies.json";
+
+interface VacancyMeta {
+  title: string;
+  description: string;
+  tasks: { title: string; description: string }[];
+}
+
+type VacancyTranslations = {
+  [lang in Lang]: VacancyMeta;
+};
+
+interface VacancyData extends VacancyTranslations {
+  id: string;
+}
+
+export interface Vacancy extends VacancyMeta {
+  id: string;
+}
+
+export function getVacancies(lang: Lang): Vacancy[] {
+  return (vacancies as VacancyData[]).map((vacancy) => ({
+    id: vacancy.id,
+    ...vacancy[lang],
+  }));
+}
