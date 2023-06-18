fd3eed2245f871be38a1c5ef069cfd291fc40a8c
slawiko <shchaurouski.slava@gmail.com>
2023-06-19 00:20:24 +0200

    Sitemap.xml

--- src/middleware.ts
+++ src/middleware.ts
@@ -32,5 +32,5 @@ export function middleware(request: NextRequest) {
 
 export const config = {
   // Matcher ignoring `/_next/` and `/api/`
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|apple-icon.png|icon.png).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|apple-icon.png|icon.png|sitemap.xml).*)"],
 };
