835ecf1182a93e9c43ff449e5f426ac4939938fb
slawiko <shchaurouski.slava@gmail.com>
2023-08-08 14:47:49 +0200

    Replaces gap with margin as it is not supported by Safari < 14.1

--- src/app/[lng]/feedback-block.tsx
+++ src/app/[lng]/feedback-block.tsx
@@ -10,9 +10,12 @@ export function FeedbackBlock(props: FeedbackBlockProps) {
   return (
     <>
       <H2 className="text-xl md:text-2xl mt-2 md:mt-4">{props.headingText}</H2>
-      <div className="flex flex-col md:flex-row flex-wrap gap-6 md:gap-8 lg:gap-12">
+      <div className="flex flex-col md:flex-row flex-wrap">
         {props.feedbacks.map((feedback, i) => (
-          <div key={i} className="grow md:basis-1/3">
+          <div key={i} className="grow md:basis-1/3 first:ml-0 first:mt-0 mt-6 md:mt-0 md:ml-8 lg:ml-12">
+            {
+              // TODO: replace margins with gap when safari 14+13.1+9.1 will be less than 0.1%
+            }
             <p className="text-sm text-justify border-l-red border-l-2 pl-4 mb-2 md:mb-3">{feedback.text}</p>
             <p className="text-xs text-right">{feedback.signature}</p>
           </div>
