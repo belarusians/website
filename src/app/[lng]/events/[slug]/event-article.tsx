2dec9d076034fdb0c2f7965ca4beaf6bc77f5956
slawiko <shchaurouski.slava@gmail.com>
2023-09-30 16:20:34 +0200

    Makes event tickets link not required

--- src/app/[lng]/events/[slug]/event-article.tsx
+++ src/app/[lng]/events/[slug]/event-article.tsx
@@ -20,15 +20,17 @@ export function EventArticle(props: ArticleProps) {
         <Image className="object-cover rounded-t-md" fill src={props.event.backgroundUrl} alt={props.event.title} />
       </div>
       <div className="flex flex-col md:flex-row gap-4 px-4 lg:px-8 pb-4 lg:pb-8 my-8 md:my-4">
-        <div className="md:basis-2/6 lg:basis-1/5">
-          <Button
-            link={props.event.ticketsLink}
-            target="_blank"
-            label={props.buttonLabel}
-            trackingName={`buy-${props.event.slug}-ticket-button`}
-            className="w-full bg-red-gradient animate-button-background-rotation bg-[length:350%_100%] text-white"
-          />
-        </div>
+        {props.event.ticketsLink && (
+          <div className="md:basis-2/6 lg:basis-1/5">
+            <Button
+              link={props.event.ticketsLink}
+              target="_blank"
+              label={props.buttonLabel}
+              trackingName={`buy-${props.event.slug}-ticket-button`}
+              className="w-full bg-red-gradient animate-button-background-rotation bg-[length:350%_100%] text-white"
+            />
+          </div>
+        )}
         <div
           className="prose-sm md:prose prose-hr:my-4 prose-a:text-red prose-a:break-words prose-blockquote:border-l-2 prose-blockquote:border-red md:basis-4/6 lg:basis-4/5"
           dangerouslySetInnerHTML={{ __html: props.event.content }}
