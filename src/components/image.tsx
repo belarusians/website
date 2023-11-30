a8c5f2127caeeaabfa87c235088306a5f3628a66
slawiko <shchaurouski.slava@gmail.com>
2023-11-30 14:44:42 +0100

    New component for Image

---
+++ src/components/image.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import Image from "next/image";
+import { useNextSanityImage } from "next-sanity-image";
+import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
+
+import { dataset, projectId } from "../sanity/env";
+import { ImageProps } from "next/dist/shared/lib/get-img-props";
+
+interface ArticleProps {
+  image: SanityImageSource;
+  alt: string;
+  className?: string;
+  fill?: boolean;
+}
+
+export function MaraImage(props: ArticleProps) {
+  const sanityImageProps = useNextSanityImage({
+    projectId,
+    dataset,
+  }, props.image);
+
+  const imgProps: ImageProps = {
+    alt: props.alt,
+    ...(
+      props.fill ? {
+        src: sanityImageProps.src,
+        loader: sanityImageProps.loader,
+      } : sanityImageProps
+    ),
+  };
+
+  return (
+    <Image
+      className={props.className}
+      {...imgProps}
+      fill={props.fill}
+    />
+  );
+}
