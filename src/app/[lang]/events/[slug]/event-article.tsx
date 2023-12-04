661c7f806de8c550006c38eb67ab0b6f69adf758
slawiko <shchaurouski.slava@gmail.com>
2023-12-04 18:34:11 +0100

    Feat: Adds tipsLink and tipsLabel to event schema: now it is possible to provide link for tips with custom text

--- src/app/[lang]/events/[slug]/event-article.tsx
+++ src/app/[lang]/events/[slug]/event-article.tsx
@@ -5,7 +5,10 @@ import { Button } from '../../../../components/button';
 interface ArticleProps {
   event: Event;
   lang: Lang;
-  buttonLabel: string;
+  ticketsLabel: string;
+  tipsLabel?: string;
+  paymentSucceeded?: boolean;
+  paymentSucceededText?: string;
 }
 
 export function EventArticle(props: ArticleProps) {
@@ -17,25 +20,74 @@ export function EventArticle(props: ArticleProps) {
           `${props.event.imageRatio ? 'aspect-video' : 'h-36 md:h-72'}`
         }
       >
-        <MaraImage className="object-cover rounded-t-md" image={props.event.backgroundUrl} fill alt={props.event.title} />
+        <MaraImage
+          className="object-cover rounded-t-md"
+          image={props.event.backgroundUrl}
+          fill
+          alt={props.event.title}
+        />
       </div>
       <div className="flex flex-col md:flex-row gap-4 px-4 lg:px-8 pb-4 lg:pb-8 my-8 md:my-4">
-        {props.event.ticketsLink && (
-          <div className="md:basis-2/6 lg:basis-1/5">
-            <Button
-              link={props.event.ticketsLink}
-              target="_blank"
-              label={props.buttonLabel}
-              trackingName={`buy-${props.event.slug}-ticket-button`}
-              className="w-full bg-red-gradient animate-bg-rotation-fast bg-[length:350%_100%] text-white"
-            />
-          </div>
-        )}
+        <div className="md:basis-2/6 lg:basis-1/5">
+          <ButtonGroup
+            slug={props.event.slug}
+            ticketsLink={props.event.ticketsLink}
+            ticketsLabel={props.ticketsLabel}
+            tipsLink={props.event.tipsLink}
+            tipsLabel={props.event.tipsLabel ?? props.tipsLabel}
+            paymentSucceeded={props.paymentSucceeded}
+            paymentSucceededText={props.paymentSucceededText}
+          />
+        </div>
         <div
           className="prose-sm md:prose prose-hr:my-4 prose-a:text-red prose-a:break-words prose-blockquote:border-l-2 prose-blockquote:border-red md:basis-4/6 lg:basis-4/5"
           dangerouslySetInnerHTML={{ __html: props.event.content }}
         ></div>
       </div>
+    </div>
+  );
+}
+
+interface ButtonGroupProps {
+  slug: string;
+  ticketsLink?: string;
+  ticketsLabel: string;
+  tipsLink?: string;
+  tipsLabel?: string;
+  paymentSucceeded?: boolean;
+  paymentSucceededText?: string;
+}
+
+function TipsButton(props: { tipsLink: string; tipsLabel?: string; slug: string }) {
+  return (
+    <Button
+      link={props.tipsLink}
+      target="_blank"
+      label={props.tipsLabel}
+      trackingName={`buy-${props.slug}-tips-button`}
+      className="w-full bg-white border-2 border-red bg-[length:350%_100%] text-red"
+    />
+  );
+}
+
+function ButtonGroup(props: ButtonGroupProps) {
+  return (
+    <div className="flex flex-col gap-4">
+      {props.paymentSucceeded && (
+        <p className="mt-4 text-center">
+          <span className="">{props.paymentSucceededText}</span>
+        </p>
+      )}
+      {!props.paymentSucceeded && props.ticketsLink && (
+        <Button
+          link={props.ticketsLink}
+          target="_blank"
+          label={props.ticketsLabel}
+          trackingName={`buy-${props.slug}-ticket-button`}
+          className="w-full bg-red-gradient animate-bg-rotation-fast bg-[length:350%_100%] text-white"
+        />
+      )}
+      {props.tipsLink && <TipsButton tipsLink={props.tipsLink} tipsLabel={props.tipsLabel} slug={props.slug} />}
     </div>
   );
 }
