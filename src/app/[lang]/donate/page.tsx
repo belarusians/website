c50003ae928a12848e2399949933cd223b94fbeb
slawiko <shchaurouski.slava@gmail.com>
2023-12-09 16:09:59 +0100

    small reformatting

--- src/app/[lang]/donate/page.tsx
+++ src/app/[lang]/donate/page.tsx
@@ -18,22 +18,23 @@ export default async function Page({ params, searchParams }: CommonPageParams & 
       <div className="flex flex-col md:flex-row gap-4 md:gap-5 lg:gap-20 xl:gap-60">
         <div className="md:basis-1/2 lg:basis-2/3 xl:basis-3/5">
           <H1>{t('heading')}</H1>
-          <p>
-            {t('text')}
-          </p>
+          <p>{t('text')}</p>
         </div>
         <div className="md:basis-1/2 lg:basis-1/3 xl:basis-2/5 grid grid-cols-2 gap-4">
-          {success ?
-            <div
-              className="col-span-2 p-2 md:p-4 lg:p-6 rounded-md bg-white shadow-lg flex items-center justify-center">
+          {success ? (
+            <div className="col-span-2 p-2 md:p-4 lg:p-6 rounded-md bg-white shadow-lg flex items-center justify-center">
               <p className="text-center">
                 {t('successMsg')}&nbsp;
                 <Link href="https://billing.stripe.com/p/login/9AQ6qpfV726XcsU144">{t('customerPortalLink')}</Link>.
               </p>
-            </div> : <DonateButtons donateBtnLabel={t('donateBtn')}
-                                    recurringLabel={t('recurring')}
-                                    donateBtnErrLabel={t('donateBtnErr')}
-            />}
+            </div>
+          ) : (
+            <DonateButtons
+              donateBtnLabel={t('donateBtn')}
+              recurringLabel={t('recurring')}
+              donateBtnErrLabel={t('donateBtnErr')}
+            />
+          )}
         </div>
       </div>
     </Section>
--- src/components/button.tsx
+++ src/components/button.tsx
@@ -27,16 +27,16 @@ export function Button(props: ButtonProps): React.ReactElement {
       </Link>
     );
   } else {
-    return (
-      <InnerButton {...props} />
-    );
+    return <InnerButton {...props} />;
   }
 }
 
 function InnerButton(props: ButtonProps) {
   return (
     <button
-      className={`transition-all ${props.size === 'large' ? 'p-2 md:p-3 lg:p-4 text-lg' : 'p-1 md:p-2 lg:p-3'} rounded-md shadow-lg ${props.disabled ? '' : 'hover:shadow-xl active:shadow-2xl'} ${props.className ?? ''}`}
+      className={`transition-all ${
+        props.size === 'large' ? 'p-2 md:p-3 lg:p-4 text-lg' : 'p-1 md:p-2 lg:p-3'
+      } rounded-md shadow-lg ${props.disabled ? '' : 'hover:shadow-xl active:shadow-2xl'} ${props.className ?? ''}`}
       disabled={props.disabled}
       onClick={props.click}
     >
