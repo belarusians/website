b8bd465c337ad7155df621aaa620546c224d29ba
slawiko <shchaurouski.slava@gmail.com>
2023-12-09 14:49:29 +0100

    EventThumbnail refactoring

--- src/app/[lang]/events-block.tsx
+++ src/app/[lang]/events-block.tsx
@@ -1,5 +1,5 @@
 import { Lang } from '../../components/types';
-import { FutureEventThumbnail } from './event-thumbnail';
+import { EventThumbnail } from './event-thumbnail';
 import { EventMeta } from '../../sanity/event/service';
 
 export interface EventsBlockProps {
@@ -7,18 +7,12 @@ export interface EventsBlockProps {
   lang: Lang;
 }
 
-function isEventPast(eventString: string): boolean {
-  return new Date(eventString).getTime() < Date.now();
-}
-
 export function EventsBlock(props: EventsBlockProps) {
-  function renderEventThumbnail(event: EventMeta, i: number) {
-    return isEventPast(event.eventDate) ? null : <FutureEventThumbnail lang={props.lang} event={event} key={i} />;
-  }
-
   return (
     <div className="flex flex-col md:flex-row justify-center gap-3 md:gap-4">
-      {props.events.map(renderEventThumbnail)}
+      {props.events.map((event, i) => (
+        <EventThumbnail lang={props.lang} event={event} key={i} />
+      ))}
     </div>
   );
 }
