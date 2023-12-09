d58ced3fd2bd6828ecb943c4c98ef11d509ef3ee
slawiko <shchaurouski.slava@gmail.com>
2023-12-09 16:19:04 +0100

    SpeedInsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Metadata } from 'next/types';
 import Script from 'next/script';
 import { Roboto } from 'next/font/google';
 import { Viewport } from 'next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 import '../components/globals.css';
 
@@ -24,31 +25,44 @@ const roboto = Roboto({
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html>
-    <head>
-      <link rel="manifest" href="/manifest.json" />
-      <link rel="manifest" href="/browserconfig.xml" />
-      <link rel="mask-icon" type="image/svg+xml" href="/icons/safari-pinned-tab.svg" color="#ed1c24" />
-      <link rel="apple-touch-icon" sizes="60x60" href="/icons/apple-touch-icon-60x60.png" />
-      <link rel="apple-touch-icon-precomposed" sizes="60x60" href="/icons/apple-touch-icon-60x60-precomposed.png" />
-      <link rel="apple-touch-icon" sizes="76x76" href="/icons/apple-touch-icon-76x76.png" />
-      <link rel="apple-touch-icon-precomposed" sizes="76x76" href="/icons/apple-touch-icon-76x76-precomposed.png" />
-      <link rel="apple-touch-icon" sizes="120x120" href="/icons/apple-touch-icon-120x120.png" />
-      <link rel="apple-touch-icon-precomposed" sizes="120x120" href="/icons/apple-touch-icon-120x120-precomposed.png" />
-      <link rel="apple-touch-icon" sizes="152x152" href="/icons/apple-touch-icon-152x152.png" />
-      <link rel="apple-touch-icon-precomposed" sizes="152x152" href="/icons/apple-touch-icon-152x152-precomposed.png" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon-180x180.png" />
-      <link rel="apple-touch-icon-precomposed" sizes="180x180" href="/icons/apple-touch-icon-180x180-precomposed.png" />
-    </head>
-    <body>
-    <Script
-      async
-      defer
-      src="https://analytics.umami.is/script.js"
-      data-website-id="d1f28365-f189-4d4c-bcea-17ee67c90f91"
-      data-domains="www.belarusians.nl"
-    />
-    <div className={roboto.className}>{children}</div>
-    </body>
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="manifest" href="/browserconfig.xml" />
+        <link rel="mask-icon" type="image/svg+xml" href="/icons/safari-pinned-tab.svg" color="#ed1c24" />
+        <link rel="apple-touch-icon" sizes="60x60" href="/icons/apple-touch-icon-60x60.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="60x60" href="/icons/apple-touch-icon-60x60-precomposed.png" />
+        <link rel="apple-touch-icon" sizes="76x76" href="/icons/apple-touch-icon-76x76.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="76x76" href="/icons/apple-touch-icon-76x76-precomposed.png" />
+        <link rel="apple-touch-icon" sizes="120x120" href="/icons/apple-touch-icon-120x120.png" />
+        <link
+          rel="apple-touch-icon-precomposed"
+          sizes="120x120"
+          href="/icons/apple-touch-icon-120x120-precomposed.png"
+        />
+        <link rel="apple-touch-icon" sizes="152x152" href="/icons/apple-touch-icon-152x152.png" />
+        <link
+          rel="apple-touch-icon-precomposed"
+          sizes="152x152"
+          href="/icons/apple-touch-icon-152x152-precomposed.png"
+        />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon-180x180.png" />
+        <link
+          rel="apple-touch-icon-precomposed"
+          sizes="180x180"
+          href="/icons/apple-touch-icon-180x180-precomposed.png"
+        />
+      </head>
+      <body>
+        <Script
+          async
+          defer
+          src="https://analytics.umami.is/script.js"
+          data-website-id="d1f28365-f189-4d4c-bcea-17ee67c90f91"
+          data-domains="www.belarusians.nl"
+        />
+        <div className={roboto.className}>{children}</div>
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
