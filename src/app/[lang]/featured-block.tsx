d858c61faed28463f995e708b65375c6c8b0dab1
slawiko <shchaurouski.slava@gmail.com>
2023-12-13 19:19:02 +0100

    some styling improvements of featured block

--- src/app/[lang]/featured-block.tsx
+++ src/app/[lang]/featured-block.tsx
@@ -14,27 +14,25 @@ export function FeaturedNewsBlock(props: FeaturedNewsBlockProps) {
   return (
     <>
       <H2>{props.headingText}</H2>
-      <div className="flex flex-wrap gap-3 md:gap-4 flex-col lg:flex-row">
+      <div className="grid grod-rows-2 grid-cols-2 lg:grid-cols-5 gap-3 md:gap-4">
         <NewsThumbnail
           lang={props.lang}
           size={'large'}
-          className="transition-all flex basis-28 md:basis-36 lg:basis-72 grow lg:grow-[2] shadow-lg hover:shadow-xl hover:scale-101"
+          className="h-36 sm:h-48 md:h-64 lg:h-96 transition-transform col-span-2 lg:col-span-3 row-span-1 lg:row-span-2 shadow-lg hover:shadow-xl hover:scale-101"
           news={props.main}
         />
-        <div className="flex flex-row lg:flex-col gap-3 md:gap-4 grow basis-28 md:basis-36">
-          <NewsThumbnail
-            lang={props.lang}
-            size={'medium'}
-            className="transition-all flex grow basis-28 md:basis-36 shadow-lg hover:shadow-xl hover:scale-101"
-            news={props.secondary[0]}
-          />
-          <NewsThumbnail
-            lang={props.lang}
-            size={'medium'}
-            className="transition-all flex grow basis-28 md:basis-36 shadow-lg hover:shadow-xl hover:scale-101"
-            news={props.secondary[1]}
-          />
-        </div>
+        <NewsThumbnail
+          lang={props.lang}
+          size={'medium'}
+          className="h-36 md:h-48 lg:h-full transition-transform col-span-1 lg:col-span-2 row-span-1 shadow-lg hover:shadow-xl hover:scale-101"
+          news={props.secondary[0]}
+        />
+        <NewsThumbnail
+          lang={props.lang}
+          size={'medium'}
+          className="h-36 md:h-48 lg:h-full transition-transform col-span-1 lg:col-span-2 row-span-1 shadow-lg hover:shadow-xl hover:scale-101"
+          news={props.secondary[1]}
+        />
       </div>
     </>
   );
--- src/app/[lang]/news-thumbnail.tsx
+++ src/app/[lang]/news-thumbnail.tsx
@@ -14,16 +14,16 @@ export interface NewsThumbnailProps {
 export async function NewsThumbnail(props: NewsThumbnailProps) {
   return (
     <div className={`rounded-md ${props.className}`}>
-      <Link className="relative flex flex-1 flex-col" href={`/${props.lang}/news/${props.news.slug}`}>
+      <Link className="h-full relative flex flex-1 flex-col" href={`/${props.lang}/news/${props.news.slug}`}>
         <MaraImage
           image={props.news.backgroundUrl}
           className="object-cover rounded-md brightness-90"
           fill
           alt={props.news.title}
         />
-        <div className="p-2 z-40 mt-auto backdrop-blur-md rounded-b-md text-white uppercase">
+        <div className="px-2 py-1 z-40 mt-auto backdrop-blur-md rounded-b-md text-white uppercase">
           <span
-            className={`text-sm ${
+            className={`text-sm inline-block ${
               props.size === 'small'
                 ? 'md:text-base lg:text-xl'
                 : props.size === 'medium'
