1d9e7a78ff11dca24b85f7ef036c5ca650e2b6fb
slawiko <shchaurouski.slava@gmail.com>
2024-03-05 20:55:46 +0100

    Resolves lint issues

--- src/components/image.tsx
+++ src/components/image.tsx
@@ -5,7 +5,6 @@ import { useNextSanityImage } from 'next-sanity-image';
 import type { SanityImageSource } from '@sanity/image-url/lib/types/types';
 
 import { dataset, projectId } from '../sanity/env';
-import { ImageProps } from 'next/dist/shared/lib/get-img-props';
 
 interface ArticleProps {
   image: SanityImageSource;
@@ -23,15 +22,12 @@ export function MaraImage(props: ArticleProps) {
     props.image,
   );
 
-  const imgProps: ImageProps = {
-    alt: props.alt,
-    ...(props.fill
-      ? {
-          src: sanityImageProps.src,
-          loader: sanityImageProps.loader,
-        }
-      : sanityImageProps),
-  };
+  const imgProps = props.fill
+    ? {
+        src: sanityImageProps.src,
+        loader: sanityImageProps.loader,
+      }
+    : sanityImageProps;
 
-  return <Image className={props.className + ' w-full'} {...imgProps} fill={props.fill} />;
+  return <Image className={props.className + ' w-full'} alt={props.alt} {...imgProps} fill={props.fill} />;
 }
