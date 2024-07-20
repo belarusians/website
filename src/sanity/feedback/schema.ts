08ebb6e1d614e82ba8a31f24b1e7b37e388e6cd1
slawiko <shchaurouski.slava@gmail.com>
2024-07-21 00:26:48 +0200

    New schema for feedback

--- src/sanity/feedback/schema.ts
+++ src/sanity/feedback/schema.ts
@@ -3,31 +3,33 @@ import { defineField, defineType } from '@sanity-typed/types';
 
 const feedback = defineType({
   name: 'feedback',
-  title: 'Feedback',
+  title: 'Водгукі',
   type: 'document',
   icon: HeartIcon,
   fields: [
     defineField({
       name: 'text',
       title: 'Text',
-      type: 'text',
+      type: 'localeText',
       validation: (Rule) => Rule.required(),
     }),
     defineField({
       name: 'signature',
       title: 'Signature',
-      type: 'string',
-      validation: (Rule) => Rule.required(),
-    }),
-    defineField({
-      // should match 'languageField' plugin configuration setting, if customized
-      name: 'language',
-      type: 'string',
-      readOnly: true,
-      hidden: true,
+      type: 'localeString',
       validation: (Rule) => Rule.required(),
     }),
   ],
+  preview: {
+    select: {
+      title: 'signature.be',
+    },
+    prepare({ title }) {
+      return {
+        title,
+      };
+    },
+  },
 });
 
 export default feedback;
--- src/sanity/feedback/service.ts
+++ src/sanity/feedback/service.ts
@@ -3,11 +3,15 @@ import { client } from '../client';
 import { Feedback } from '../../../sanity.config';
 
 export async function getNRandomFeedbacksByLang(lang: Lang, n: number): Promise<Feedback[]> {
-  const allIds: { _id: string }[] = await client.fetch(`*[_type == "feedback" && language == "${lang}"]{ _id }`);
+  const allIds: { _id: string }[] = await client.fetch('*[_type == "feedback"]{ _id }');
   const ids = allIds
     .sort(() => Math.random() - Math.random())
     .slice(0, n)
     .map(({ _id }) => _id);
 
-  return client.fetch('*[_id in $ids]', { ids });
+  return client.fetch(`*[_id in $ids]{
+    _id,
+    "text": text.${lang},
+    "signature": signature.${lang},
+   }`, { ids });
 }
